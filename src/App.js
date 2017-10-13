fe95063616116da2b589f47bff6015bd3b768a3b
maksymko658 <Jmaksy@ukr.net>
2017-10-13 09:41:14 +0300

    add items on page

--- src/App.js
+++ src/App.js
@@ -32,12 +32,11 @@ handleSubmit(e) {
     username: ''
   });
 }
-//We need to actually grab those items from our Firebase database so that we can store them into our state.
-itemsRef.on('value', (snapshot) => {
-  console.log(snapshot.val());
-});
-/*We'll attach this event listener inside of our componentDidMount, 
-so that we start tracking our Potluck items as soon as our component loads on to the page:*/
+/*Here, we instantiate a new array and populate it with the results that come back from our value listener.
+ We for…in over each key, and push the result into an object inside our newState array. 
+ Finally, once all the keys are iterated over 
+ (and therefore all items are grabbed from our database), 
+ we update the state with this list of items from our database.*/
 componentDidMount() {
   const itemsRef = firebase.database().ref('items');
   itemsRef.on('value', (snapshot) => {
@@ -53,6 +52,12 @@ componentDidMount() {
     this.setState({
       items: newState
     });
+    //We need to actually grab those items from our Firebase database so that we can store them into our state.
+itemsRef.on('value', (snapshot) => {
+  console.log(snapshot.val());
+/*We'll attach this event listener inside of our componentDidMount, 
+so that we start tracking our Potluck items as soon as our component loads on to the page:*/
+});
   });
 }
     render() {
@@ -67,6 +72,22 @@ componentDidMount() {
     <button onSubmit={this.handleSubmit} type="form" class="btn btn-outline-primary btn-lg btn-block">Primary</button>
     </form>
   </div>
+    <div className="container-fluid">
+    <section className='display-item'>
+  <div className="wrapper">
+    <ul>
+      {this.state.items.map((item) => {
+        return (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            <p>brought by: {item.user}</p>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
+</section>
+    </div>
 </div>
     );
   }
